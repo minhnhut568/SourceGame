b0588853570f9946b1d9cc13e2c8349ce7eea4f0
Minh Nhựt <minhnhut568@github.com>
2020-12-05 16:57:25 +0700

    remove some file

--- SourceGame/PlayerMiniBullet.cpp
+++
@@ -1,7 +0,0 @@
-#include "PlayerMiniBullet.h"
-#include "SpriteManager.h"
-PlayerMiniBullet::PlayerMiniBullet()
-{
-	setSprite(SpriteManager::getInstance()->getSprite(SPRITE_INFO_PLAYER_MINI_BULLET));
-	animationGameTime.init(500);
-}
